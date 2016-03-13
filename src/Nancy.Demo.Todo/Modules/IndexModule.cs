d22a259a2d01e45c357658aa232e3e25333e1146
feeeper <shirobokov.andrey@gmail.com>
2016-03-13 21:47:17 +0400

    init

---
+++ src/Nancy.Demo.Todo/Modules/IndexModule.cs
@@ -0,0 +1,10 @@
+﻿namespace Nancy.Demo.Todo.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = _ => View["index"];
+        }
+    }
+}
